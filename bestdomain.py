bc2ef930332de7bdd0d5266fef8879b3a001b14e
liaoxian95175141 <143249390+liaoxian95175141@users.noreply.github.com>
2024-12-17 22:26:05 +0800

    Update bestdomain.py

--- bestdomain.py
+++ bestdomain.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     
     # 示例URL和子域名对应的IP列表
     subdomain_ip_mapping = {
-        'bestcf': 'https://ipdb.030101.xyz/api/bestcf.txt',
+        'yx': 'https://raw.liaoxian.top/vip.txt?token=liaoxiansiku',
         'bestproxy': 'https://ipdb.030101.xyz/api/bestproxy.txt',
         # 添加更多子域名和对应的IP列表URL
     }
